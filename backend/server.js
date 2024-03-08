1c96c9f28dea9e570044d7a63394aed12005c88f
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2024-03-08 15:58:14 +0530

    improved working of game , add all logic processing to server side

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,27 @@
-const io = require("socket.io")(3000, {
+import Patterns from "./Patterns.js";
+import { Server } from "socket.io";
+
+const io = new Server(3000, {
   cors: ["https://localhost:5173"],
 });
 
 const players = [];
+let player = "x";
+let boardS = [];
+let winner;
+let roomIdLocal;
+
+const checkDraw = () => {
+  let filled = true;
+  boardS.forEach((val) => {
+    if (val == "") filled = false;
+  });
+
+  if (filled) {
+    return true;
+  }
+  return false;
+};
 
 io.on("connection", (socket) => {
   console.log(`Connection established with ${socket.id}`);
@@ -15,22 +34,56 @@ io.on("connection", (socket) => {
     );
     console.log(`The players in the room are ${players}`);
     if (players.length == 2) {
-      socket.to(roomId).emit("your-turn", players[0]);
+      socket.to(roomId).emit("your-turn", players, player);
       console.log(players[0] + " your turn!");
+      player = "o";
     }
   });
 
   socket.on("move-made", (board, roomId) => {
-    console.log(board);
+    roomIdLocal = roomId;
+    const checkPattern = () => {
+      Patterns.forEach((currentPattern) => {
+        const firstPlayer = boardS[currentPattern[0]];
+        if (firstPlayer == "") return;
+        let foundWin = true;
+        currentPattern.forEach((idx) => {
+          if (boardS[idx] != firstPlayer) {
+            foundWin = false;
+          }
+        });
+        if (foundWin) {
+          winner = firstPlayer;
+          io.sockets.in(roomId).emit("win", winner);
+        }
+      });
+    };
+
     console.log(`${socket.id} made the move`);
+    boardS = board;
+    console.log(boardS);
     if (roomId == "") {
       console.log("No room is available"); //can so socket.broadcast.emit() to give this message to users
     } else {
       const curr_player = players.filter((player) => {
         return player != socket.id;
       });
-      socket.to(roomId).emit("recieve-board", board, curr_player);
+      if (checkDraw()) {
+        socket.to(roomId).emit("draw");
+      }
+      checkPattern();
+      socket.to(roomId).emit("recieve-board", board, curr_player, player);
+      player = player == "x" ? "o" : "x";
       console.log(`Turn of ${curr_player}`);
     }
+  });
+  socket.on("restart", () => {
+    socket.to(roomIdLocal).emit("restart-called", players);
+    console.log("Restart called");
+    console.log(players);
+
+    socket.to(roomIdLocal).emit("your-turn", players, "x");
+    console.log(players[0] + " your turn!");
+    player = "o";
   });
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Square from "./components/Square";
-import Patterns from "./Patterns";
 
 import { io } from "socket.io-client";
 
@@ -12,75 +11,79 @@ socket.on("connect", function () {
 
 const App = () => {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-  const [player, setPlayer] = useState("x");
-  const [result, setResult] = useState({ winner: "none", state: "none" });
+  const [player, setPlayer] = useState("");
   const [gameFinished, setGameFinished] = useState(false);
   const [RoomId, setRoomId] = useState("");
   const [myTurn, setmyTurn] = useState(false);
+  const [isDraw, setisDraw] = useState(false);
+  const [Winner, setWinner] = useState(null);
+  const [players, setplayers] = useState([]);
 
-  socket.on("your-turn", (myid) => {
-    if (myid == socket.id) {
+  socket.on("your-turn", (myid, player) => {
+    setplayers(myid);
+    if (myid[0] == socket.id) {
       setmyTurn(true);
+      setPlayer(player);
     }
   });
 
-  socket.on("recieve-board", (boardRecieved, myId) => {
+  socket.on("recieve-board", (boardRecieved, myId, player) => {
     if (myId[0] == socket.id) {
-      console.log(myId[0], socket.id);
+      console.log(myId[0] + " - is me");
       setBoard(boardRecieved);
       console.log(`The board recieved is ${boardRecieved}`);
+      setPlayer(player);
       setmyTurn(true);
     }
   });
+
+  socket.on("draw", function () {
+    setisDraw(true);
+  });
+
+  socket.on("win", (winner) => {
+    setGameFinished(true);
+    setWinner(winner);
+  });
+
+  socket.on("restart-called", (p) => {
+    console.log("Restart call recieved");
+    console.log(p);
+    if (p[0] == socket.id || p[1] == socket.id) {
+      setGameFinished(false);
+      setBoard(["", "", "", "", "", "", "", "", ""]);
+      setWinner(null);
+      setisDraw(false);
+    }
+  });
+
+  const restartGame = () => {
+    console.log("restart call to server");
+    console.log("Restart sent to player - " + players);
+    socket.emit("restart", players);
+    setGameFinished(false);
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setWinner(null);
+    setisDraw(false);
+  };
 
   const squareClick = (square) => {
     if (gameFinished) return;
-
+    let tboard = [];
+    // console.log(player);
     myTurn &&
-      setBoard(
-        board.map((val, index) => {
-          if (index == square && val == "") return player;
-          return val;
-        })
-      );
-    myTurn && setPlayer(player === "x" ? "o" : "x");
-    myTurn && socket.emit("move-made", board, RoomId);
-    console.log(`This board is sent ${board}`);
-    // setmyTurn(false);
-  };
-
-  const checkPattern = () => {
-    Patterns.forEach((currentPattern) => {
-      const firstPlayer = board[currentPattern[0]];
-      if (firstPlayer == "") return;
-      let foundWin = true;
-      currentPattern.forEach((idx) => {
-        if (board[idx] != firstPlayer) {
-          foundWin = false;
+      (tboard = board.map((val, index) => {
+        if (index == square && val == "") {
+          return player;
         }
-      });
-      if (foundWin) {
-        setResult({ winner: firstPlayer, state: "won" });
-      }
-    });
-  };
-  const checkDraw = () => {
-    let filled = true;
-    board.forEach((val) => {
-      if (val == "") filled = false;
-    });
 
-    if (filled) {
-      return true;
-    }
-    return false;
-  };
-
-  const restartGame = () => {
-    setBoard(["", "", "", "", "", "", "", "", ""]);
-    setPlayer("x");
-    setResult({ winner: "none", state: "none" });
-    setGameFinished(false);
+        return val;
+      }));
+    // console.log(tboard);
+    myTurn && socket.emit("move-made", tboard, RoomId);
+    console.log(`This board is sent ${tboard}`);
+    setmyTurn(false);
+    setBoard(tboard);
   };
 
   // useEffect(() => {
@@ -185,28 +188,33 @@ const App = () => {
           val={board[8]}
         />
       </div>
-      {checkDraw() && (
+      {isDraw && (
         <h1 className=" text-3xl font-semibold mt-4 text-yellow-600">
           It was a Draw!
         </h1>
       )}
-
-      {result.state != "none" && (
+      {myTurn && !gameFinished && (
+        <h1 className=" text-3xl font-semibold mt-4 text-yellow-600">
+          Your Turn!
+        </h1>
+      )}
+      {gameFinished && (
         <h1 className=" text-3xl font-semibold mt-4 text-green-600">
-          {result.winner.toUpperCase()} won the game.
+          {Winner.toUpperCase()} won the game.
         </h1>
       )}
 
-      {gameFinished && (
-        <button
-          onClick={() => {
-            restartGame();
-          }}
-          className=" px-4 py-1 bg-slate-200 text-neutral-800 mt-4 rounded-md text-3xl font-semibold"
-        >
-          Restart
-        </button>
-      )}
+      {((gameFinished ||
+        isDraw) && (
+          <button
+            onClick={() => {
+              restartGame();
+            }}
+            className=" px-4 py-1 bg-slate-200 text-neutral-800 mt-4 rounded-md text-3xl font-semibold"
+          >
+            Restart
+          </button>
+        ))}
     </div>
   );
 };
